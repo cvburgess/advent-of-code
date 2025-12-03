7f5bb094059be4884c3ec1ca5bcdab7b4e84ec8c
Charles Burgess <consulting@cvburgess.com>
2025-12-02 23:19:29 -0500

    Day 2

---
+++ 2025/02.ts
@@ -0,0 +1,75 @@
+import { Range, ranges } from "./02-input.ts";
+
+// Challenge: https://adventofcode.com/2025/day/2
+
+const sum = (arr: number[]) => arr.reduce((prev, cur) => prev + cur, 0);
+
+const getInvalidIdsForRangePart1 = (range: Range) => {
+  const [start, end] = range;
+  const numIds = (end - start) + 1;
+
+  const allIds = Array
+    .from({ length: numIds })
+    .map((_, index) => index + start);
+
+  return allIds.filter((id) => {
+    const numChars = id.toString().length;
+
+    const isOddNumberOfChars = numChars % 2 !== 0;
+    if (isOddNumberOfChars) return false;
+
+    const halfWayIndex = numChars / 2;
+    const firstHalf = id.toString().slice(0, halfWayIndex);
+    const secondHalf = id.toString().slice(halfWayIndex);
+    const isRepeatingSequence = firstHalf === secondHalf;
+
+    return isRepeatingSequence;
+  });
+};
+
+const invalidIdsPart1 = ranges.flatMap(getInvalidIdsForRangePart1);
+
+console.log(sum(invalidIdsPart1));
+
+const splitStringByLength = (str: string, length: number) => {
+  const subStrings = [];
+
+  for (let i = 0; i < str.length; i += length) {
+    subStrings.push(str.slice(i, i + length));
+  }
+
+  return subStrings;
+};
+
+const getInvalidIdsForRangePart2 = (range: Range) => {
+  const [start, end] = range;
+  const numIds = (end - start) + 1;
+
+  const allIds = Array
+    .from({ length: numIds })
+    .map((_, index) => index + start);
+
+  return allIds.filter((id) => {
+    const idStr = id.toString();
+    const numChars = idStr.length;
+    let isValid = true;
+
+    for (
+      let subStrLength = 1;
+      (subStrLength <= Math.floor(numChars / 2));
+      subStrLength++
+    ) {
+      if (isValid) {
+        if (numChars % subStrLength) continue;
+        const subStrings = splitStringByLength(idStr, subStrLength);
+        isValid = subStrings.some((subString) => subString !== subStrings[1]);
+      }
+    }
+
+    return !isValid;
+  });
+};
+
+const invalidIdsPart2 = ranges.flatMap(getInvalidIdsForRangePart2);
+
+console.log(sum(invalidIdsPart2));
